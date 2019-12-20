f94cd62f2c3c09e590c5c109f85d2434da257269
Liu Tianqing <liutianqing@dfsoft.com.cn>
2019-12-20 17:52:23 +0800

    'init'

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,51 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <section class="header">
+      头部
+    </section>
+    <section class="main">
+      <div class="sidebar">
+        <router-link to="/" class="nav-home">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </div>
+      <div class="content">
+        <router-view/>
+      </div>
+    </section>
   </div>
 </template>
 
-<style lang="scss">
+<style scoped lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.header {
+  text-align: center;
+  font-size: 20px;
+  height: 150px;
+  line-height: 100px;
+  border: 1px solid #CCC;
+}
+.main {
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  border: 1px solid red;
+}
+.sidebar {
+  width: 100px;
+  border: 1px solid blue;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .nav-home {
+    margin-bottom: 120px;
   }
+}
+.content {
+  margin-left: 20px;
 }
 </style>
